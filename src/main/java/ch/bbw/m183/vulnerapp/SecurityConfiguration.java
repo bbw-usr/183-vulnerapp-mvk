9a7c130ddaf6b2dfe40b38ca84d129011b72453b
Jean-Luc Jordi <jean-luc.jordi@digitecgalaxus.ch>
2026-04-09 16:45:16 +0200

    comit

---
+++ src/main/java/ch/bbw/m183/vulnerapp/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package ch.bbw.m183.vulnerapp;
+
+import ch.bbw.m183.vulnerapp.repository.UserRepository;
+import ch.bbw.m183.vulnerapp.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository){
+        return username -> {
+            return userRepository.findById(username)
+                    .map(x -> new User(x.getUsername(), x.getPassword(), List.of()))
+                    .orElseThrow();
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(
+                request -> request.requestMatchers("/api/**")
+                        .authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/blog"))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
