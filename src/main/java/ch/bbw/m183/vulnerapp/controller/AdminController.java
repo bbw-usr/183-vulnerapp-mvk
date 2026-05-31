73832cb50bbd7adcafd495afb5a15a668964a326
Jean-Luc Jordi <jean-luc.jordi@digitecgalaxus.ch>
2026-05-31 11:43:46 +0200

    tests

--- src/main/java/ch/bbw/m183/vulnerapp/controller/AdminController.java
+++ src/main/java/ch/bbw/m183/vulnerapp/controller/AdminController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin123") // noone will ever guess!
+@RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class AdminController {
 
